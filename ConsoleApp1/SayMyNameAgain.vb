6e2db7d5ae3b2e13f219fb355360920f5935d874
rasset69@gmail.com <rasset69@gmail.com>
2022-01-30 18:43:10 -0700

    Change modual name

--- ConsoleApp1/SayMyNameAgain.vb
+++ ConsoleApp1/SayMyNameAgain.vb
@@ -2,56 +2,50 @@
 'RCET0265
 'Spring 2022
 'Say My Name Again
-'
+'https://github.com/SethRas/ConsoleApp1.git
 
 Option Explicit On
 Option Strict On
 Option Compare Text
 
-Module Thing
+Module SayMyNameAgain
 
     Sub Main()
-
 
         Dim name As String
         Dim runAgain As Boolean
 
         Do
 
-
             Console.WriteLine("Your name?")
             name = Console.ReadLine()
 
+            Dim comment1 As String
+            Dim comment2 As String
+            Dim comment3 As String
+            Dim comment4 As String
 
-            Dim message As String
-            Dim message1 As String
-            Dim message2 As String
-            Dim message3 As String
-
-            message = name & " Cuoio!"
-            message1 = name & " I'm Sorry, please take me back!"
-            message2 = name & " Aren't you glad this isn't labVIEW?"
-            message3 = name & " Look I did it!!!"
-
-
+            comment1 = name & " Cuoio!"
+            comment2 = name & " I'm Sorry, please take me back!"
+            comment3 = name & " Aren't you glad this isn't labVIEW?"
+            comment4 = name & " Look I did it!!!"
 
             Select Case name
 
-
                 Case "Joe"
-                    Console.WriteLine(message)
+                    Console.WriteLine(comment1)
                     runAgain = True
                 Case "Emily"
-                    Console.WriteLine(message1)
+                    Console.WriteLine(comment2)
                     runAgain = True
                 Case "Seth"
-                    Console.WriteLine(message2)
+                    Console.WriteLine(comment3)
                     runAgain = True
                 Case "Tim"
-                    Console.WriteLine(message3)
+                    Console.WriteLine(comment4)
                     runAgain = True
                 Case Else
-                    Console.WriteLine("Who dat boi? Who him is?")
+                    Console.WriteLine("Who are you? What are you doing here?")
                     runAgain = False
 
             End Select
